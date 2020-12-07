487811851e77ac99910b09f86c9b4c89099aa05c
Akinkunmi <akinkunmiisrael41@gmail.com>
2020-12-07 12:32:37 +0000

    project started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { FaBeer } from 'react-icons/fa';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3> Lets go for a <FaBeer />? </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
